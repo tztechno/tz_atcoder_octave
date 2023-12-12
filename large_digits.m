4d1eb9d5ad663ed4201404608b4f7ee963a5faf1
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-12 13:40:29 +0900

    Update large_digits.m

--- large_digits.m
+++ large_digits.m
@@ -4,9 +4,9 @@ input_vals = str2num(input_str);
 A = input_vals(1);
 B = input_vals(2);
 
-A2 = arrayfun(@(ai) str2double(ai), num2str(A) - '0');
-B2 = arrayfun(@(bi) str2double(bi), num2str(B) - '0');
+A_digits = arrayfun(@str2double, num2str(A));
+B_digits = arrayfun(@str2double, num2str(B));
 
-ans_value = max(sum(A2), sum(B2));
+ans_value = max(sum(A_digits), sum(B_digits));
 
 disp(ans_value);
