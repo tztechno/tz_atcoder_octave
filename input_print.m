cfce224a780e8d32fdbb3f42fe6c3e9a585b5f62
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-03-24 14:48:34 +0900

    Update input_print.m

--- input_print.m
+++ input_print.m
@@ -7,3 +7,16 @@ A = str2num(input("Enter A (space-separated): ", "s"));
 % NとAを出力
 disp(N);
 disp(A);
+
+
+################################
+
+% Nを入力
+N = input("");
+
+% Aを入力
+A = str2num(input("", "s"));
+
+% NとAを出力
+disp(N);
+disp(A);
